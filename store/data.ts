f484ee7f93fa1ed3de93e29cf333b4f93e5d25b2
zb09 <zbdigital009@gmail.com>
2022-11-14 10:16:14 +0800

    dockerfile

--- store/data.ts
+++ store/data.ts
@@ -1,12 +1,15 @@
 import { defineStore } from 'pinia'
 
 export const DataStore = defineStore('data', () => {
+  /* state */
   const count = ref<number>(0)
 
+  /* getter */
   const GetCount = () => {
     return count.value
   }
 
+  /* actions */
   const SetCount = (val: number) => {
     count.value = val
   }
